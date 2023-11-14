f36ba6ed725f7e9a1b142224effed7d1c22e22c5
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-11-14 06:53:19 -0500

    Update stolen_vehicles_queries.sql

--- 1.) Data Playground/1.) Guided Projects/2.) Intermediate/1.) Motor Vehicle Thefts (Intermediate - Excel, MySQL)/stolen_vehicles_queries.sql
+++ 1.) Data Playground/1.) Guided Projects/2.) Intermediate/1.) Motor Vehicle Thefts (Intermediate - Excel, MySQL)/stolen_vehicles_queries.sql
@@ -7,6 +7,7 @@ SELECT
     YEAR(date_stolen) AS year,
     COUNT(*) AS num_vehicles
 FROM stolen_vehicles
+WHERE vehicle_type IS NOT NULL
 GROUP BY year;
 
 -- Find the total number of vehicles stolen each month
@@ -14,13 +15,14 @@ SELECT
     LEFT(MONTHNAME(date_stolen), 3) AS month,
     COUNT(*) AS num_vehicles
 FROM stolen_vehicles
+WHERE vehicle_type IS NOT NULL
 GROUP BY MONTH(date_stolen)
 ORDER BY MONTH(date_stolen);
 
 -- Find the total number of vehicles stolen each day of the week
 SELECT
     CASE
-    	WHEN WEEKDAY(date_stolen) = 0 THEN 'Mon'
+	WHEN WEEKDAY(date_stolen) = 0 THEN 'Mon'
         WHEN WEEKDAY(date_stolen) = 1 THEN 'Tue'
         WHEN WEEKDAY(date_stolen) = 2 THEN 'Wed'
         WHEN WEEKDAY(date_stolen) = 3 THEN 'Thu'
@@ -30,6 +32,7 @@ SELECT
 	END AS day_of_week,
     COUNT(*) AS num_vehicles
 FROM stolen_vehicles
+WHERE vehicle_type IS NOT NULL
 GROUP BY WEEKDAY(date_stolen)
 ORDER BY WEEKDAY(date_stolen);
 
@@ -37,34 +40,26 @@ ORDER BY WEEKDAY(date_stolen);
 -- Objective 2: Identify what vehicles were stolen
 -- -----------------------------------------------
 
--- Find the top 10 stolen vehicle types
-SELECT
-    vehicle_type,
-    COUNT(*) AS num_vehicles
-FROM stolen_vehicles
-WHERE vehicle_type IS NOT NULL
-GROUP BY vehicle_type
-ORDER BY num_vehicles DESC
-LIMIT 10;
-
 -- Find the total number of vehicles stolen per make type
 SELECT
     m.make_type,
     COUNT(*) AS num_vehicles
 FROM stolen_vehicles s
 LEFT JOIN make_details m ON s.make_id = m.make_id
-WHERE m.make_type IS NOT NULL
+WHERE s.vehicle_type IS NOT NULL
 GROUP BY m.make_type
 ORDER BY num_vehicles DESC;
 
 -- Create a pivot table with the following dimensions:
 -- rows representing the top 10 stolen vehicle types
--- columns representing the each make types (with an additional column total make types)
+-- columns representing the each make type ratio and total number of vehicles stolen
 -- values representing the total number of vehicles stolen
 SELECT
     s.vehicle_type,
-    COUNT(CASE WHEN m.make_type = 'Standard' THEN s.vehicle_id ELSE NULL END) AS standard,
-    COUNT(CASE WHEN m.make_type = 'Luxury' THEN s.vehicle_id ELSE NULL END) AS luxury,
+    ROUND(COUNT(CASE WHEN m.make_type = 'Standard' THEN s.vehicle_id ELSE NULL END)/
+		COUNT(*), 2) AS standard,
+    ROUND(COUNT(CASE WHEN m.make_type = 'Luxury' THEN s.vehicle_id ELSE NULL END)/
+		  COUNT(*), 2) AS luxury,
     COUNT(*) AS total
 FROM stolen_vehicles s
 LEFT JOIN make_details m ON s.make_id = m.make_id
@@ -85,8 +80,9 @@ SELECT
     COUNT(*) AS num_vehicles
 FROM stolen_vehicles s
 LEFT JOIN locations l ON s.location_id = l.location_id
+WHERE s.vehicle_type IS NOT NULL
 GROUP BY
-    l.region,
+	l.region,
     l.population,
     l.density
 ORDER BY num_vehicles DESC;
